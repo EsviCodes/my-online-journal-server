36e4cc6534f1494ee354e84452a1ec7b8909db77
EsviCodes <esther.js.vis@gmail.com>
2019-10-22 17:39:12 +0200

    Added auth middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { toJWT, toData } = require("./jwt");
+const auth = require("./auth-middleware");
 
 const router = new Router();
 
@@ -39,26 +40,10 @@ router.post("/login", (req, res, next) => {
     });
 });
 
-router.get("/secret-endpoint", (req, res) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-      res.send({
-        message: "Thanks for visiting the secret endpoint.",
-        data
-      });
-    } catch (error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`
-      });
-    }
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials"
-    });
-  }
+router.get("/secret-endpoint", auth, (req, res) => {
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`
+  });
 });
 
 module.exports = router;
